cbc8a2f52a99a58335e1ca852ffdacec05687cc1
Utkarshini Acharya <utkarshini.acharya.2398@gmail.com>
2021-06-28 23:14:03 +0530

    Add MovieCard component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <button @click="fetchData">fetch</button>
+  <MovieCard />
 </template>
 
 <script>
+import MovieCard from "~/components/MovieCard";
+
 export default {
+  components: {
+    MovieCard
+  },
   created() {
-    this.$axios
-      .get("/3/movie/123?api_key=" + process.env.tmdbAPIKey)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err));
+    // this.$axios
+    //   .get("/3/movie/123?api_key=" + process.env.tmdbAPIKey)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => console.log(err));
   }
 };
 </script>
