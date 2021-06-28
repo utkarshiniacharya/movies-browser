b9404c25ab7d2d62385a10795f6016ac96e55c97
Utkarshini Acharya <utkarshini.acharya.2398@gmail.com>
2021-06-28 23:51:56 +0530

    Hide arrow keys in Carousel

--- components/BackdropCarousel.vue
+++ components/BackdropCarousel.vue
@@ -5,6 +5,7 @@
     :cycle=true
     interval="3000"
     height="100vh"
+    :show-arrows=false
   >
     <v-carousel-item v-for="(color, i) in colors" :key="color">
       <v-sheet :color="color" height="100%" tile>
