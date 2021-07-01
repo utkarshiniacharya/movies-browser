deeea8364826bdf67e59c8922d53ae37ab3c7a4b
Utkarshini Acharya <utkarshini.acharya.2398@gmail.com>
2021-07-02 02:59:48 +0530

    Update styles for TypeHeading component

--- components/TypeHeading.vue
+++ components/TypeHeading.vue
@@ -3,6 +3,7 @@
       <h1 class="heading-name">
           Upcoming
       </h1>
+      <div class="name-stroke" />
   </div>
 </template>
 
@@ -11,7 +12,16 @@ export default {
 
 }
 </script>
-
-<style>
-
+<style scoped>
+.heading-type {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 3rem 0;
+}
+.name-stroke {
+  background-color: white;
+  width: 2.5rem;
+  height: 4px;
+}
 </style>
