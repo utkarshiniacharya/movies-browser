f67fc9f96836f872efcf2eaedcbe92c186feafd9
Utkarshini Acharya <utkarshini.acharya.2398@gmail.com>
2021-07-02 00:11:04 +0530

    Update state

--- components/BackdropCarousel.vue
+++ components/BackdropCarousel.vue
@@ -1,30 +1,51 @@
 <template>
-  <v-carousel
-    v-model="model"
-    :hide-delimiters=true
-    :cycle=true
-    interval="3000"
-    height="100vh"
-    :show-arrows=false
-  >
-    <v-carousel-item v-for="(color, i) in colors" :key="color">
-      <v-sheet :color="color" height="100%" tile>
-        <v-row class="fill-height" align="center" justify="center">
-          <div class="text-h2">Slide {{ i + 1 }}</div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
+  <v-container>
+    <v-carousel
+      v-model="model"
+      :hide-delimiters=true
+      :cycle=true
+      interval="3000"
+      height="100vh"
+      :show-arrows=false
+      v-if="mediaTypeSelected == mediaTypes.movie"
+    >
+      <v-carousel-item v-for="(color, i) in colors" :key="color">
+        Movies
+        {{ i }}
+      </v-carousel-item>
+    </v-carousel>
+    <v-carousel
+      v-model="model"
+      :hide-delimiters=true
+      :cycle=true
+      interval="3000"
+      height="100vh"
+      :show-arrows=false
+      v-if="mediaTypeSelected == mediaTypes.tv"
+    >
+      <v-carousel-item v-for="(color, i) in colors" :key="color">
+        TV
+        {{ i }}
+      </v-carousel-item>
+    </v-carousel>
+  </v-container>
 </template>
 
 <script>
+import { mediaTypes } from '~/constants/mediaTypes';
+import {mapGetters, mapActions } from 'vuex';
+
 export default {
   data() {
     return {
       model: 0,
-      colors: ['red'],
-      trendingMedia: []
+      colors: ['red', 'green'],
+      trendingMedia: [],
+      mediaTypes: mediaTypes
     }
+  },
+  computed: {
+    ...mapGetters(['mediaTypeSelected'])
   }
 };
 </script>
