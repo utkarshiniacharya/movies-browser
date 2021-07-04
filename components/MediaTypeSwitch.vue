2111c3a3dac097333f5cd162fef334ca39f23eec
Utkarshini Acharya <utkarshini.acharya.2398@gmail.com>
2021-07-05 00:22:46 +0530

    Create MediaTypeSwitch component

---
+++ components/MediaTypeSwitch.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1 class="heading-name">Movies</h1>
+      <div class="name-stroke" />
+      <h1 class="heading-name">TV Shows</h1>
+      <div class="name-stroke" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.name-stroke {
+    background-color: white;
+    width: 2.5rem;
+    height: 3px;
+}
+.heading-name {
+    margin-top: 2rem;
+}
+</style>
