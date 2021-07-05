e172f1698461a9cbf6541d07b5e1d8037a46c201
Utkarshini Acharya <utkarshini.acharya.2398@gmail.com>
2021-07-05 22:49:31 +0530

    Update person details page

--- pages/person/_id/index.vue
+++ pages/person/_id/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-      {{ this.personName }}
+  <div class="person-details">
+    <div>
+        <img :src="`https://image.tmdb.org/t/p/original${personPhotoURL}`" alt="No image available." class="poster" v-if="personPhotoURL !== null">
+        <div class="null-poster" v-else />
+    </div>
+    <div class="person-info">
+        <h2 class="person-name">{{ personName}}</h2>
+        <h3 class="heading">Biography</h3>
+        <p class="person-biography">{{ biography }}</p>
+    </div>
   </div>
 </template>
 
@@ -10,9 +18,7 @@ export default {
         return {
             personPhotoURL: '',
             personName: '',
-            biography: '',
-            birthDay: '',
-            placeOfBirth: ''
+            biography: ''
         }
     },
     methods: {
@@ -20,12 +26,9 @@ export default {
             return new Promise((resolve, reject) => {
                 this.$axios.get("/3/person/" + this.$route.params.id + "?api_key=" + process.env.tmdbAPIKey)
                     .then(response => {
-                        console.log(response.data.profile_path);
                         this.personPhotoURL = "https://image.tmdb.org/t/p/original" + response.data.profile_path;
                         this.personName = response.data.name;
                         this.biography = response.data.biography;
-                        this.birthDay = response.data.birthday;
-                        this.placeOfBirth = response.data.place_of_birth;
                         resolve(response.data)
                     })
                     .catch(error => {
@@ -41,5 +44,39 @@ export default {
 </script>
 
 <style scoped>
-
+.person-details {
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+    justify-content: center;
+    max-width: 900px;
+    margin: auto;
+}
+.person-info {
+    margin: 2rem;
+}
+.poster {
+    border-radius: 5%;
+    height: 250px;
+    width: 170px;
+    z-index: -1;
+    object-fit: cover;
+}
+.null-poster {
+  border-radius: 5%;
+  height: 250px;
+  width: 170px;
+  background-color: gray;
+  opacity: 0.3;
+}
+@media only screen and (max-width: 960px) {
+    .person-details {
+        flex-direction: column;
+        text-align: justify;
+    }
+    .person-name {
+        margin-bottom: 2rem;
+        text-align: center;
+    }
+}
 </style>
